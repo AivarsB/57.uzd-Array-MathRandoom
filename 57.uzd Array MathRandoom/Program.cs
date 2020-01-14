2d03260fcc523252235658ac21e6286e56a795d3
AivarsB <aivars.berzins@inbox.lv>
2020-01-14 17:37:16 +0200

    Add project files.

---
+++ 57.uzd Array MathRandoom/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _57.uzd_Array_MathRandoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] randomNumbers = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Random rnd = new Random();
+                int dice = rnd.Next(1, 7);
+                randomNumbers[i] = dice;
+            }
+            foreach (var number in randomNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
+// Definēt int masīvu, kura lielums ir 10. No koda piešķirt visiem masīva elementiem vērtības. 
+// Var izmantot Math.Random metodi.
